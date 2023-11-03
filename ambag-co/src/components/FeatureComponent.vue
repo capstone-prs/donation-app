52b7b262e96fa5873a6bd15aa8e265d29a4166ed
Jenny Rose Suelan <jennyrose.suelan-20@cpu.edu.ph>
2023-11-03 11:53:27 +0800

    Add home and project page initial UI

---
+++ ambag-co/src/components/FeatureComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <div
+    class="row q-mb-lg q-pa-md"
+    style="background-color: teal; border-radius: 10px; width: 100%"
+  >
+    <div class="col">
+      <div class="text-h5" style="color: white">TOP ALL TIME DONOR:</div>
+      <DonorAvatar />
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import DonorAvatar from './DonorAvatar.vue';
+</script>
--- ambag-co/src/layouts/HeaderLayout.vue
+++ ambag-co/src/layouts/HeaderLayout.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="row">
-    <div class="col">
+    <div class="col q-mb-xl">
       <q-fab
         class="transparent"
         text-color="teal"
         icon="menu"
         direction="down"
         vertical-actions-align="left"
+        @click="toggleFab"
+        :class="dynamicMargin"
       >
         <q-fab-action
           square
@@ -45,10 +47,31 @@
 </template>
 
 <script setup lang="ts">
+import { computed, ref } from 'vue';
 import { useRouter } from 'vue-router';
 
 const router = useRouter();
+const isFabOpen = ref(false);
+
+const toggleFab = () => {
+  isFabOpen.value = !isFabOpen.value;
+};
+
+const dynamicMargin = computed(() => {
+  if (isFabOpen.value == true) {
+    return 'push-below';
+  } else {
+    return '';
+  }
+});
+
 const navigateTo = (page: string) => {
   router.push(`/${page}`);
 };
 </script>
+
+<style>
+.push-below {
+  margin-bottom: 150px;
+}
+</style>
