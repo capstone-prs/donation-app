f62d154931b2171982ba15c9b9150bc900a8cae3
iforgot-my-username <garibay2018paul@gmail.com>
2023-11-07 08:09:32 +0800

    🚧 Fixes disappearing user info when reloading

--- ambag-co/src/pages/ProfilePage.vue
+++ ambag-co/src/pages/ProfilePage.vue
@@ -10,20 +10,21 @@
     </q-header>
     <q-page-container class="q-pt-xl centered-container">
       <div class="centered-content">
-        <q-avatar
-          size="100px"
-          font-size="52px"
-        > <img :src="userProfilePic || ''" alt=""></q-avatar>
+        <q-avatar size="100px" font-size="52px">
+          <img :src="currentUser?.photoURL || ''" alt=""
+        /></q-avatar>
       </div>
       <div class="q-pt-md centered-content">
-        <span style="color: grey">{{ userEmail }}</span>
+        <span style="color: grey">{{ currentUser?.email }}</span>
         <h6 style="color: grey">Projects Started</h6>
         <h5 style="color: teal">90</h5>
         <h6 style="color: grey">Donations Made</h6>
         <h5 style="color: teal">90 ETH</h5>
       </div>
       <div class="q-pt-md centered-content">
-        <q-btn rounded color="teal-5" size="lg" @click="handleLogOut">LOGOUT</q-btn>
+        <q-btn rounded color="teal-5" size="lg" @click="handleLogOut"
+          >LOGOUT</q-btn
+        >
       </div>
     </q-page-container>
   </q-layout>
@@ -31,32 +32,39 @@
 
 <script setup lang="ts">
 import { useRouter } from 'vue-router';
-import { logout} from 'src/utils/firebase';
-import { getAuth } from 'firebase/auth';
+import { logout } from 'src/utils/firebase';
+import { User, getAuth } from 'firebase/auth';
+import { onBeforeMount, ref } from 'vue';
+import app from 'src/boot/firebase';
 
+const currentUser = ref<User | null>(null);
 
-const userEmail = getAuth().currentUser?.email
-const userProfilePic = getAuth().currentUser?.photoURL
 const router = useRouter();
 const navigateBack = () => {
   router.go(-1);
 };
 
+onBeforeMount(() =>
+  getAuth(app)
+    .authStateReady()
+    .then(() => {
+      currentUser.value = getAuth(app).currentUser;
+    })
+);
+
 const handleLogOut = () => {
   logout()
-  .then(()=> {
-    console.log('working!')
-    router.push('/#')
-  })
-  .catch(()=>{
-    console.error('logout fail');
-  })
-}
-
+    .then(() => {
+      console.log('working!');
+      router.push('/#');
+    })
+    .catch(() => {
+      console.error('logout fail');
+    });
+};
 </script>
 <style>
 .centered-container {
-
   display: relative;
   justify-content: center;
   align-items: center;
